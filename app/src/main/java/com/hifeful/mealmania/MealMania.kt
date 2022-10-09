3fc3ff891541245ec0d2b58cf30ceacf2614cc10
Maksym Trushevych <maksym.trushevych@trendyol.com>
2022-10-09 20:03:26 +0300

    Init commit

---
+++ app/src/main/java/com/hifeful/mealmania/MealMania.kt
@@ -0,0 +1,12 @@
+package com.hifeful.mealmania
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MealMania : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meal_mania)
+    }
+}
---
+++ app/src/main/java/com/hifeful/mealmania/home/HomeFragment.kt
@@ -0,0 +1,8 @@
+package com.hifeful.mealmania.home
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+
+}
